0c9afc0ed6e7c6a359e1ab6b49e65b18abd74dfc
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-03-24 13:25:18 -0500

    Rewriting main.c to be a state machine. Should work much better than the previous design

--- src/main.c
+++ src/main.c
@@ -81,50 +81,31 @@ uint8_t D_config = 0;
  * Note: It is not what is sent to the ADC. It is 4 bits in the word that will
  * be sent.
  */
-uint8_t config = 0;
+
 uint8_t reset_word = 0;
 uint16_t temp = 0;
 bool next = false;
 bool CNV_IsHigh = true;
 
-void check_TMR2(uint32_t status, uintptr_t context){
-    CNV_Clear();
-    while(BUSY_Get());
-    RDL_Clear();
-    SPI1_Write(&reset_word, 1);
-//    while(SPI1_IsBusy());
+int tmr1_counter = 0;
+void TMR1_InterruptRoutine(uint32_t status, uintptr_t context){
+    tmr1_counter = tmr1_counter + 1;
 }
 
-void reset_ADC(){
-    RESET_Set();
-    TMR3_Start();
+bool tmr2_alarm = false;
+void TMR2_InterruptRoutine(uint32_t status, uintptr_t context){
+    tmr2_alarm = true;
 }
 
-void check_TMR3(uint32_t status, uintptr_t context){
-    RESET_Clear();
-    RDL_Set();
-    CNV_Set();
-    TMR2_Start();
-}
 
-void check_TMR1(uint32_t status, uintptr_t context){
-    CNV_IsHigh = false;
-    TMR1_Stop();
-}
-
-/**
- * main
- * @return 
- */
 int main ( void )
 {
     /* Initialize all modules */
     SYS_Initialize ( NULL );
     
     /* Initialize TMR2 Callback Register for 10kHz Frequency*/
-//    TMR1_CallbackRegister(check_TMR1, (uintptr_t) NULL);
-//    TMR2_CallbackRegister(check_TMR2, (uintptr_t) NULL);
-//    TMR3_CallbackRegister(check_TMR3, (uintptr_t) NULL);
+    TMR1_CallbackRegister(TMR1_InterruptRoutine, (uintptr_t) NULL);
+    TMR2_CallbackRegister(TMR2_InterruptRoutine, (uintptr_t) NULL);
     
     /* Configure PGAs refer to PGA datasheet for combinations */
     configureA(A_config, A_shdn);
@@ -137,6 +118,104 @@ int main ( void )
     RESET_Clear();
     RDL_Set();
     
+    
+    /**
+    * \section Possible Configuration Word Table
+    * | channel | BINARY  | INTEGER  |
+    * |---------|---------|----------|
+    * |   A     | X010011 |     19   |
+    * |   B     | X010111 |     23   |
+    * |   C     | X011011 |     27   |
+    * |   D     | X011111 |     31   |
+    * @return 
+    */
+    int config_words[4] = {19, 23, 27, 31};
+    uint8_t curr_channel = config_words[0];
+    typedef enum{
+                A,
+                B,
+                C,
+                D,
+                END_OF_LIST
+    } CHANNEL; // 4 channels for the 4 different sensors
+    CHANNEL CURR_CHANNEL = A;
+    
+
+    typedef enum{
+                START,
+                CONVERSION,
+                WRSPI_STATE,
+                WAIT_SPI,
+                WAIT_STATE,
+                BUSY_STATE,
+                END
+    }STATE;
+    
+    STATE CURR_STATE = START;
+    STATE NEXT_STATE = START;
+    int wait_time;
+    STATE AFTER_WAIT_STATE;
+    uint16_t data; // Used to store 2 bytes of data from ADC
+    
+    while(true){
+        
+        switch(CURR_STATE){
+            case START:
+                TMR2_Start();
+                CNV_Set();
+                wait_time = 20; //20 ns
+                AFTER_WAIT_STATE = CONVERSION;
+                NEXT_STATE = WAIT_STATE;
+            
+            case CONVERSION:
+                CNV_Clear();
+                NEXT_STATE = BUSY_STATE;
+                
+            case BUSY_STATE:
+                if(!BUSY_Get()){
+                    NEXT_STATE = WAIT_STATE;
+                    wait_time = 1; //1 ns
+                    AFTER_WAIT_STATE = WRSPI_STATE;
+                    RDL_Clear();
+                }
+            
+            case WRSPI_STATE:
+                SPI1_WriteRead(&curr_channel, 2, &data, 2);
+                NEXT_STATE = WAIT_SPI;
+            
+            case WAIT_SPI:
+                if(!SPI1_IsBusy()){
+                    NEXT_STATE = END;
+                    RDL_Set(); 
+                }
+            
+            case WAIT_STATE:
+                tmr1_counter = 0;
+                TMR1_Start();
+                if(tmr1_counter == wait_time){
+                    NEXT_STATE = AFTER_WAIT_STATE;
+                    TMR1_Stop();
+                }
+                
+            case END:
+                if(tmr2_alarm){
+                    NEXT_STATE = START;
+                    TMR2_Stop();
+                    CURR_CHANNEL = CURR_CHANNEL + 1;
+                    if(CURR_CHANNEL == END_OF_LIST){
+                        CURR_CHANNEL = A;
+                    }
+                    curr_channel = config_words[CURR_CHANNEL];
+                }
+                
+        }
+        
+        CURR_STATE = NEXT_STATE; // Sets up the Next State
+        // NEXT_STATE = CURR_STATE; // This is assumed if NEXT_STATE is not changed
+    }
+    
+    
+    //
     uint16_t ch78config = (28 << 8 );
     while(true){
         
