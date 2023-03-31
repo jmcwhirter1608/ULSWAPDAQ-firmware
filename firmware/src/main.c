f68f897d94449dc7c236ba7869ed1a0aa16e36c1
jmcwhirter1608 <78233755+jmcwhirter1608@users.noreply.github.com>
2023-03-31 17:09:23 -0500

    Corrected main.c Experiencing Bugs with CNV being set to high. Doesn't look like a software issue though. Might need to resolder ADC for proper alignment because CNV gets set to high in a pattern. Busy doesn't get read anymore.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -87,14 +87,19 @@ uint16_t temp = 0;
 bool next = false;
 bool CNV_IsHigh = true;
 
-int tmr1_counter = 0;
+bool tmr1_alarm = false;
 void TMR1_InterruptRoutine(uint32_t status, uintptr_t context){
-    tmr1_counter = tmr1_counter + 1;
+    tmr1_alarm = true;
 }
 
 bool tmr2_alarm = false;
 void TMR2_InterruptRoutine(uint32_t status, uintptr_t context){
     tmr2_alarm = true;
+}
+
+bool tmr3_alarm = false;
+void TMR3_InterruptRoutine(uint32_t status, uintptr_t context){
+    tmr3_alarm = true;
 }
 
 
@@ -106,6 +111,11 @@ int main ( void )
     /* Initialize TMR2 Callback Register for 10kHz Frequency*/
     TMR1_CallbackRegister(TMR1_InterruptRoutine, (uintptr_t) NULL);
     TMR2_CallbackRegister(TMR2_InterruptRoutine, (uintptr_t) NULL);
+    TMR3_CallbackRegister(TMR3_InterruptRoutine, (uintptr_t) NULL);
+    
+    TMR1_Start();
+    TMR2_Start();
+    TMR3_Start();
     
     /* Configure PGAs refer to PGA datasheet for combinations */
     configureA(A_config, A_shdn);
@@ -119,6 +129,7 @@ int main ( void )
     RDL_Set();
     
     
+    
     /**
     * \section Possible Configuration Word Table
     * | channel | BINARY  | INTEGER  |
@@ -129,7 +140,7 @@ int main ( void )
     * |   D     | X011111 |     31   |
     * @return 
     */
-    int config_words[4] = {19, 23, 27, 31};
+    int config_words[4] = {19 << 8, 23 << 8, 27 << 8, 31 << 8};
     uint8_t curr_channel = config_words[0];
     typedef enum{
                 A,
@@ -146,14 +157,14 @@ int main ( void )
                 CONVERSION,
                 WRSPI_STATE,
                 WAIT_SPI,
-                WAIT_STATE,
+                WAIT_STATE_TMR1,
+                WAIT_STATE_TMR3,
                 BUSY_STATE,
                 END
     }STATE;
     
     STATE CURR_STATE = START;
     STATE NEXT_STATE = START;
-    int wait_time;
     STATE AFTER_WAIT_STATE;
     uint16_t data; // Used to store 2 bytes of data from ADC
     
@@ -161,23 +172,24 @@ int main ( void )
         
         switch(CURR_STATE){
             case START:
-                TMR2_Start();
+                tmr2_alarm = false;
                 CNV_Set();
-                wait_time = 20; //20 ns
                 AFTER_WAIT_STATE = CONVERSION;
-                NEXT_STATE = WAIT_STATE;
+                NEXT_STATE = WAIT_STATE_TMR1;
+                tmr1_alarm = false;
             
             case CONVERSION:
                 CNV_Clear();
                 NEXT_STATE = BUSY_STATE;
                 
             case BUSY_STATE:
-                if(!BUSY_Get()){
-                    NEXT_STATE = WAIT_STATE;
-                    wait_time = 1; //1 ns
-                    AFTER_WAIT_STATE = WRSPI_STATE;
-                    RDL_Clear();
-                }
+                while(BUSY_Get());
+                NEXT_STATE = WAIT_STATE_TMR3;
+                tmr3_alarm = false;
+                AFTER_WAIT_STATE = WRSPI_STATE;
+//                    NEXT_STATE = WRSPI_STATE;
+                RDL_Clear();
+                
             
             case WRSPI_STATE:
                 SPI1_WriteRead(&curr_channel, 2, &data, 2);
@@ -186,27 +198,53 @@ int main ( void )
             case WAIT_SPI:
                 if(!SPI1_IsBusy()){
                     NEXT_STATE = END;
-                    RDL_Set(); 
+                }
+                else{
+                    NEXT_STATE = WAIT_SPI;
                 }
             
-            case WAIT_STATE:
-                tmr1_counter = 0;
-                TMR1_Start();
-                if(tmr1_counter == wait_time){
+            case WAIT_STATE_TMR1:
+                LED1_Set();
+                if(tmr1_alarm){
                     NEXT_STATE = AFTER_WAIT_STATE;
                     TMR1_Stop();
                 }
+                else{
+                    NEXT_STATE = WAIT_STATE_TMR1;
+                }
+                
+            case WAIT_STATE_TMR3:
+                TMR3_Start();
+                if(tmr3_alarm){
+                    NEXT_STATE = AFTER_WAIT_STATE;
+                    TMR3_Stop();
+                }
+                else{
+                    NEXT_STATE = WAIT_STATE_TMR3;
+                }
                 
             case END:
+                RDL_Set();
                 if(tmr2_alarm){
                     NEXT_STATE = START;
-                    TMR2_Stop();
+//                    TMR2_Stop();
+                    tmr2_alarm = false;
                     CURR_CHANNEL = CURR_CHANNEL + 1;
                     if(CURR_CHANNEL == END_OF_LIST){
                         CURR_CHANNEL = A;
                     }
                     curr_channel = config_words[CURR_CHANNEL];
                 }
+                else{
+                    NEXT_STATE = END;
+                }
+                
+//            default:
+//                RESET_Set();
+//                for(int i = 0; i < 100; i++);
+//                RESET_Clear();
+//                RDL_Set();
+//                NEXT_STATE = START;
                 
         }
         
@@ -214,138 +252,12 @@ int main ( void )
         // NEXT_STATE = CURR_STATE; // This is assumed if NEXT_STATE is not changed
     }
     
+    LED1_Clear();
+    LED2_Clear();
+    LED3_Clear();
     
     //
-    uint16_t ch78config = (28 << 8 );
-    while(true){
-        
-        CNV_Set();
-//        while(!CNV_Get()); //Delay
-        for(int i = 0; i < 500; i++);//Delay
-        CNV_Clear();
-//        for(int i = 0; i < 25; i++);//Delay
-        while(BUSY_Get());
-        RDL_Clear(); 
-        for(int i = 0; i < 50; i++);
-//        while(BUSY_Get());
-        SPI1_WriteRead(&ch78config, 2, &temp, 2);
-                       
-        while(SPI1_IsBusy());
-        RDL_Set();
-        while(!RDL_Set());
-//        CNV_Set();
-//        RDL_Set();
-//        for(int i = 0; i < 1000; i++);
-//        CNV_Clear();
-//        
-//        while(BUSY_Get());
-//        RDL_Clear();
-//        SPI1_Read(&temp, 2);
-//        while(SPI1_IsBusy());
-        
-        
-        
-    };
     
-    /* Reset ADC */
-//    reset_ADC();
-//    
-//    RESET_Set();
-//    RESET_Clear();
-//    RDL_Set();
-//    CNV_Set();
-//    for(int i = 0; i < 1000; i++);
-//    CNV_Clear();
-//    while(BUSY_Get());
-//    RDL_Clear();
-//    SPI1_Write(&reset_word, 1);
-//    //while(SPI1_IsBusy());
-//
-//    RDL_Set();
-//    while(true){
-//        // Write Result onto UART1 
-//        char tx_buffer[100];
-//        sprintf(tx_buffer, "Hello World");
-//        UART1_Write(&tx_buffer, strlen(tx_buffer));
-//    };
-//    
-//    
-//    /* Program Sequencer */
-//
-////    for( uint8_t i = 0; i < 15; i++){
-////        // configuration word is config left shifted by 3 bits plus
-////        //  the input range selection bits (10) and Digital Gain Compression (0)
-////        uint8_t config_word = (i << 3) + 4; // 0x0AAAA100 where A is config
-////        RDL_Set();
-////        CNV_Set();
-////        CNV_Clear();
-////        while(BUSY_Get());
-////        RDL_Clear();
-////        SPI1_Write(&config_word, 1);
-////        while(SPI1_IsBusy());
-////    }
-//    
-//    /* Read From ADC*/
-//    while ( true )
-//    {
-//        /**
-//         * If statements will skip Mux channels if they are not populated
-//         * Note: If we are on this state and this state's PGA is shutdown, skip
-//         */
-//        if(pga1A_shdn && config == 0){ 
-//            config = config + 1; 
-//        } 
-//        if(pga1B_shdn && config == 1){
-//            config = config + 1;
-//        }
-//        if(pga2A_shdn && config == 2){
-//            config = config + 1;
-//        }
-//        if(pga2B_shdn && config == 3){
-//            config = 0;
-//        }
-//        // configuration word is config left shifted by 3 bits plus
-//        //  the input range selection bits (10) and Digital Gain Compression (0)
-//        //uint8_t config_word = (config << 3) + 4; // 0x0AAAA100 where A is config
-//        //RDL_Set();
-//        //CNV_Set();
-//        //CNV_Clear();
-//        //while(BUSY_Get());
-//        //RDL_Clear();
-//        //SPI1_Write(&config_word, 1);
-//        //while(SPI1_IsBusy());
-//        
-//        char miso_buff[3];
-//        RDL_Set();
-//        CNV_Set();
-//        CNV_Clear();
-//        while(BUSY_Get());
-//        RDL_Clear();
-//        SPI1_Read(&miso_buff, 3);
-//        while(SPI1_IsBusy());
-//        
-//        // Split Received Buffer into Data and Configuration word
-//        uint16_t rxdata = (miso_buff[2] << 8) + miso_buff[1];
-//        
-//        // Create float percentage of rxdata and multiply it by 
-//        //  Input Range from Data Sheet [-4.096, 4.096]
-//        float txdata = (rxdata / 65535.0) * 8.192 - 4.096;
-//        
-//        // Write Result onto UART1 
-//        char tx_buffer[50];
-//        sprintf(tx_buffer, "%0.2f", txdata);
-//        UART1_Write(&tx_buffer, strlen(tx_buffer));
-//        
-//        // Increment config
-//        config = config + 1; 
-//        while(!next);
-//        next = false;
-//        /* Maintain state machines of all polled MPLAB Harmony modules. */
-//        SYS_Tasks ( );
-//    }
-//
-//    /* Execution should not come here during normal operation */
-
     return ( EXIT_FAILURE );
 }
 
